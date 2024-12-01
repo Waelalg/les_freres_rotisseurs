ab6e31c4f50020ca728762a804ddda4b40c427c0
stoneAdz <azrouadlene@gmail.com>
2024-12-01 01:10:39 +0100

    Update MenuCard.jsx

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -19,7 +19,7 @@ const MenuItemCard = ({ item }) => {
 
         {/* Price */}
         <div className="text-center text-xl font-bold mb-4 text-orange-600">
-          ${item.price.toFixed(2)}
+          €{item.price.toFixed(2)}
         </div>
       </div>
     </div>
