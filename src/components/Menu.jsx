bbd04429c1fee09fdbc13b89bd7da7dca7d9f87f
stoneAdz <azrouadlene@gmail.com>
2024-12-01 01:36:23 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,79 +1,32 @@
-import React from 'react'
-import MenuItemCard from './MenuCard'
-import img1 from "../assets/galeri/88f3f9e0-5a02-4c66-a4c7-7d1f752b5369.jpg"
-import img2 from "../assets/galeri/IMG_8149.jpeg"
+import React from 'react';
+import MenuItemCard from './MenuCard';
+import img1 from "../assets/galeri/88f3f9e0-5a02-4c66-a4c7-7d1f752b5369.jpg";
 
 const Menu = () => {
   const menuItems = [
-    {
-      id: 1,
-      name: "Poulet rôti",
-      image: img1,
-      price: 11.50,
-      rating: 4.5
-    },
-    {
-      id: 2,
-      name: "Demi Poulet rôti",
-      image: img1,
-      price: 6.00,
-      rating: 4.0
-    },
-    {
-      id: 3,
-      name: "Poulet fermier",
-      image: img1,
-      price: 18.90,
-      rating: 4.2
-    },
-    {
-      id: 4,
-      name: "Demi Poulet fermier",
-      image: img1,
-      price: 9.50,
-      rating: 4.3
-    },
-    {
-      id: 4,
-      name: "Cuisse de poulet",
-      image: img1,
-      price: 3.50,
-      rating: 4.3
-    },
-    {
-      id: 4,
-      name: "Saucisse de volaille",
-      image: img1,
-      price: 2.50,
-      rating: 4.3
-    },
-    {
-      id: 4,
-      name: "Pain oriental",
-      image: img1,
-      price: 1.20,
-      rating: 4.3
-    },
-    {
-      id: 4,
-      name: "Pot de sauce",
-      image: img1,
-      price: 1.50,
-      rating: 4.3
-    }
-  ]
+    { id: 1, name: "Poulet rôti", image: img1, price: 11.50, rating: 4.5 },
+    { id: 2, name: "Demi Poulet rôti", image: img1, price: 6.00, rating: 4.0 },
+    { id: 3, name: "Poulet fermier", image: img1, price: 18.90, rating: 4.2 },
+    { id: 4, name: "Demi Poulet fermier", image: img1, price: 9.50, rating: 4.3 },
+    { id: 5, name: "Cuisse de poulet", image: img1, price: 3.50, rating: 4.3 },
+    { id: 6, name: "Saucisse de volaille", image: img1, price: 2.50, rating: 4.3 },
+    { id: 7, name: "Pain oriental", image: img1, price: 1.20, rating: 4.3 },
+    { id: 8, name: "Pot de sauce", image: img1, price: 1.50, rating: 4.3 },
+  ];
 
   return (
-  <div className="grid grid-cols-4 gap-6">
-    {menuItems.map((item, index) => (
-      <div key={item.id} className={`menu-item ${index === 1 || index === 4 ? 'clear-left' : ''}`}>
-        <MenuItemCard item={item} />
-      </div>
-    ))}
-  </div>
-)
+    <div className="grid grid-cols-4 gap-6">
+      {menuItems.map((item, index) => (
+        <React.Fragment key={item.id}>
+          {/* Carte du menu */}
+          <MenuItemCard item={item} />
 
-}
+          {/* Forcer un retour à la ligne après les items 2, 4, 6, 7, et 8 */}
+          {[1, 3, 5, 6, 7].includes(index) && <div className="col-span-full"></div>}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
 
-export default Menu
-
+export default Menu;
