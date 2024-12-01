2efd57178abb63dd8445f91a4e86a13e98e94e98
stoneAdz <azrouadlene@gmail.com>
2024-12-01 01:27:04 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -66,7 +66,7 @@ const Menu = () => {
   return (
 
       
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
           {menuItems.map(item => (
             <MenuItemCard key={item.id} item={item} />
           ))}
